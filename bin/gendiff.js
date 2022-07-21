ddc3a7020225afa738c6998c7054abd5ed583e0b
rinat-u <rinat1kam@mail.ru>
2022-07-22 02:41:10 +0500

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
