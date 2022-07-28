2c4db518c32ebd5cd50ace5650e58d15a48cc594
rinat-u <rinat1kam@mail.ru>
2022-07-28 09:43:17 +0500

    fix linter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,16 @@
+import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 test('genDiff-1', () => {
   expect(genDiff('../__fixtures__/file1.json', '../__fixtures__/file2.json'))
-  .toBe(
-  `{
+    .toBe(
+      `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-}`
-  );
+}`,
+    );
 });
